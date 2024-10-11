a77d6345e715ba6672bceb0f6dd3d3ccc4702f4a
split <split@split.pet>
2024-10-11 15:34:55 -0700

    fix: :bug: remove extra period

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "node:path"
 export default defineConfig({
     build: {
-        outDir: "../dist",
+        outDir: "./dist",
         rollupOptions: {
             input: {
                 index: resolve(__dirname, "index.html")
