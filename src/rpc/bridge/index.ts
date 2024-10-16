88260df8215b6f44f8a46d1fdaaaaba65ec05adf
split <split@split.pet>
2024-10-16 13:25:12 -0700

    feat(rpc-child): :sparkles: implement CrChild

--- src/rpc/bridge/index.ts
+++ src/rpc/bridge/index.ts
@@ -1,4 +1,20 @@
-import browser from "webextension-polyfill"
-
+import * as Standard from "../standard.js"
+import type { z } from "zod"
 const frame = document.createElement("iframe")
 frame.src = "https://discord.com/humans.txt?__feather_rpc_slave"
+
+chrome.runtime.onMessage.addListener(message => {
+    if (message.target != "bridge") return // this isn't meant for us
+
+    // we can assume that this is probably a PTCRequest
+    const { request }: { request: z.infer<typeof Standard.PTCRequest> } =
+        message
+
+    // okay, let's send this PTCRequest down to the child
+    frame.contentWindow?.postMessage(request, "https://discord.com")
+})
+
+window.addEventListener("message", ({ data }) => {
+    // let's send this up to our background to be processed
+    chrome.runtime.sendMessage({})
+})
--- src/rpc/parent/adapters/chrome.ts
+++ src/rpc/parent/adapters/chrome.ts
@@ -1,17 +1,52 @@
 import { Child } from "./shared.js"
+import { z } from "zod"
+import * as Standard from "../../standard.js"
 
 export class CrChild extends Child {
     constructor() {
         super()
+        // Let's listen to messages from a potential document
+        chrome.runtime.onMessage.addListener(({ target, request }) => {
+            if (target != "browser") return // this isn't for us
+
+            // check if this is a good CTPRequest
+            const { success, data: final } =
+                Standard.CTPRequest.safeParse(request)
+
+            if (!success) return // return if it's not
+            this.processRequest(final) // process if it is
+        })
     }
 
-    async spawn() {}
+    async spawn() {
+        const {
+            offscreen: { Reason },
+            runtime: { ContextType },
+        } = chrome
 
-    async destroy() {
-        throw new Error(`not implemented for CrChild`)
+        // Check first if we have any open offscreen documents
+        const contexts = await chrome.runtime.getContexts({
+            contextTypes: [ContextType.OFFSCREEN_DOCUMENT],
+        })
+
+        if (contexts.length > 0) return
+
+        // Looks like we don't. Let's make a new one.
+        return chrome.offscreen.createDocument({
+            url: chrome.runtime.getURL("/src/rpc/bridge/index.html"),
+            reasons: [Reason.IFRAME_SCRIPTING],
+            justification: "to be used to communicate with the Discord gateway",
+        })
     }
 
-    sendRequest() {
-        throw new Error(`not implemented for CrChild`)
+    destroy() {
+        return chrome.offscreen.closeDocument()
+    }
+
+    sendRequest(request: z.infer<typeof Standard.PTCRequest>) {
+        return chrome.runtime.sendMessage({
+            target: "bridge",
+            request,
+        })
     }
 }
