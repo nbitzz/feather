28a87c1423611d51b1472d019ee5cad2920f76ff
split <split@split.pet>
2024-10-16 20:06:18 -0700

    refactor(rpc-child): :recycle: reorganize child

--- src/rpc/child/discord/index.ts
+++
@@ -1,9 +0,0 @@
-import Gateway from "./gateway/Gateway.js"
-import getToken from "./util/getToken.js"
-const gateway = new Gateway()
-const token = getToken()
-
-if (!token) {
-} else {
-    await gateway.login(token)
-}
---
+++ src/rpc/child/index.ts
@@ -0,0 +1,4 @@
+import Gateway from "./discord/gateway/Gateway.js"
+import getToken from "./discord/util/getToken.js"
+const gateway = new Gateway()
+const token = getToken()
